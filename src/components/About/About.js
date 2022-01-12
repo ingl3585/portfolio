34a18c2ca35614400017d415eebc4ba8a0fa4951
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2022-01-12 13:27:47 -0700

    minor styling updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,21 +12,15 @@ const About = () => {
 			</div>
 			<div className='a-right'>
 				<h1 className='a-title'>About Me</h1>
-				{/* <p className='a-sub'>A meteorologist turned full stack developer</p> */}
 				<p className='a-desc'>
-					I'm a meteorologist turned full stack developer based in Denver,
-					Colorado. I am an avid lover of technology and decided to launch
-					myself officially into the world of development in early July 2021.
-					Early on, I have enjoyed programming and continued to learn languages
-					such as: HTML, CSS, JavaScript, and also Python. Soon, I will expand
-					that list further as I love to learn and continue to advance my
-					knowledge in the CS space. As much as I love developing and anything
-					to do with computers/consoles, I am an outdoorsman at heart and enjoy
-					being outside and doing anything along the lines of hiking,
-					snowboarding, fishing, camping, hanging out with friends, and also
-					making new ones along the way. Thank you for taking the time to look
-					at my portfolio, and if there is anything you need, please let me know
-					in the form down below!
+					I'm a meteorologist turned full stack developer based in Denver, CO. I
+					am an avid lover of technology and decided to launch myself officially
+					into the world of development in early July 2021. Early on, I have
+					enjoyed programming and continued to learn languages such as: HTML,
+					CSS, JavaScript, and also Python. Soon, I will expand that list
+					further as I love to learn and continue to advance my knowledge in the
+					CS space. Thank you for taking the time to look at my portfolio, and
+					if there is anything you need, please let me know down below!
 				</p>
 			</div>
 		</div>
--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -9,28 +9,25 @@ const ProjectList = () => {
 			<div className='pl-texts'>
 				<h1 className='pl-title'>Project List</h1>
 				<p className='pl-desc'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus,
-					magna nec sagittis mattis, turpis felis elementum lectus, a facilisis
-					justo dolor sagittis nulla. Aenean et fringilla quam, ac blandit
-					turpis. Nunc pellentesque at orci ac interdum. Phasellus lobortis
-					tortor.
+					Here a a few projects I have been working on or have worked on in the
+					past. Check back soon for more projects!
 				</p>
 			</div>
 			<div className='pl-list'>
-				<div className='p-item'>
-					<a
-						href='https://ingl3585.github.io/weather-app/'
-						target='_blank'
-						rel='noreferrer'>
-						<img className='p-img' src={WeatherApp} alt='weather-app-gif' />
-					</a>
-				</div>
 				<div className='p-item'>
 					<a
 						href='https://ingl3585.github.io/total-weather-frontend/'
 						target='_blank'
 						rel='noreferrer'>
 						<img className='p-img' src={TotalWeather} alt='total-weather-gif' />
+					</a>
+				</div>
+				<div className='p-item'>
+					<a
+						href='https://ingl3585.github.io/weather-app/'
+						target='_blank'
+						rel='noreferrer'>
+						<img className='p-img' src={WeatherApp} alt='weather-app-gif' />
 					</a>
 				</div>
 				<div className='p-item'>
